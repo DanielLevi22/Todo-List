cfda2b3dcb3a0898effe3c8b9638922a9f4cd4d5
Daniel Levi <daniellevi000@gmail.com>
2023-06-15 22:29:33 -0300

    "fist"

---
+++ src/List/List.ts
@@ -0,0 +1,16 @@
+import { ListType } from "@/types/ListType";
+
+export const lista: ListType[] = [
+    {
+        id: 1,
+        name: 'Primara Tarefa',
+        active: false,
+       
+    },
+    {
+        id: 2,
+        name: 'Segunda Tarefa',
+        active: false,
+        
+    },
+];
---
+++ src/types/ListType.ts
@@ -0,0 +1,6 @@
+
+export type ListType = {
+    id: number
+    name: string
+    active: boolean    
+}
